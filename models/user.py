eed9643cdf02895d2e580604a325818b3b4b309e
mag222mag <mag222mag@yandex.ru>
2026-08-22 16:02:07 +0300

    feature: (User, Login) add yandex oauth

--- models/user.py
+++ models/user.py
@@ -6,6 +6,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 class UserProfile(Base):
     __tablename__ = "UserProfile"
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
-    username: Mapped[str] = mapped_column(nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=True)
+    password: Mapped[str] = mapped_column(nullable=True)
+    google_access_token: Mapped[Optional[str]]
+    yandex_access_token: Mapped[Optional[str]]
+    email: Mapped[Optional[str]]
+    name: Mapped[Optional[str]]
+
     
